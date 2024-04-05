d419a6f18fff13367fcfe1af1bee2296beb0a6c2
unknown <t3ma2002@mail.ru>
2024-04-05 15:40:39 +0300

    all up

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from 'types'
+
+export const defaultUser: LocalGithubUser = {
+	login: 'artemiy228',
+	avatar: 'https://avatars.githubusercontent.com/u/34098193?v=4',
+	name: 'Artem Dudukchyan',
+	company: 'EPAM',
+	blog: '',
+	location: 'Russia/Sochi',
+	bio: 'React developer',
+	twitter: '',
+	repos: 11,
+	followers: 2,
+	following: 3,
+	created: '2017-11-29T13:56:14Z',
+}
